f5814bc9c55380a87272d22c923de62a160919f8
Leonardo-mb <109990386+Leonardo-mb@users.noreply.github.com>
2023-05-19 16:20:05 -0300

    Feat: Implementar javascript basico

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 console.log("Rondando sistema de gerenciamento de jogos.");
+function setText(valor) {
+    for (let i = 0; i <= valor; i++) {
+      let p = document.createElement('p');
+      p.innerText = "Valor: " + i;
+      document.body.appendChild(p);
+    }
+  }
+
+setText(10);
